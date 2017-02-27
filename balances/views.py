0c57b59c20c6604bc5a0752db45a11944baf2df9
justhineT <jusatumb@gmail.com>
2017-02-27 14:14:19 -0500

    asiento contable

--- balances/views.py
+++ balances/views.py
@@ -4,29 +4,56 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Cuenta
 from .models import Asiento
+
+from django.db.models import F
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 def index(request):
     cuenta = Cuenta.objects.all()
     template = loader.get_template("balances/index.html")
-    #contexto = Context({'cuenta':cuenta})
     context = {
         'cuenta': cuenta,
-
     }
     return HttpResponse(template.render(context))
 
 def asiento(request):
-    asiento = Asiento.objects.all()
+    asiento= list(Asiento.objects.all())
+    num=1
+    for asiento in asiento:
+        print num
+        asiento = Asiento.objects.all().get(id=num)
+        asiento.ctadebito.saldo = F("saldo") - asiento.total
+        asiento.ctacredito.saldo = F("saldo") + asiento.total
+        asiento.ctadebito.save(update_fields=["saldo"])
+        asiento.ctacredito.save(update_fields=["saldo"])
+        num=num+1
+
+    asientos = Asiento.objects.all()
     modelObjects = list(Asiento.objects.all())
     total=0
     for modelObject in modelObjects:
+        # Cuenta.objects.filter(descripcion=modelObject.ctadebito).update(saldo=saldo - asiento.total)
         total = total + modelObject.total
-    print total
     template = loader.get_template("balances/asiento.html")
     context = {
-        'asiento': asiento,
+        'asientos': asientos,
         'total': total,
+    }
+    return HttpResponse(template.render(context))
+
+def cambioSaldo(request):
+    asiento = Asiento.objects.all().get(id=1)
+    asiento.ctadebito.saldo = F("saldo") - asiento.total
+    asiento.ctacredito.saldo = F("saldo") + asiento.total
+    asiento.ctadebito.save(update_fields=["saldo"])
+    asiento.ctacredito.save(update_fields=["saldo"])
+
+    template = loader.get_template("balances/index.html")
+    #contexto = Context({'cuenta':cuenta})
+    context = {
+        'cuenta': cuenta,
+
     }
     return HttpResponse(template.render(context))
